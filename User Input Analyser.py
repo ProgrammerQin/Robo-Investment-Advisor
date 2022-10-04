7357f080194b5093221048f890aff8c3e080bb15
qinyang <qinyang789@gmail.com>
2022-10-03 21:44:34 -0400

    Oct 1st

--- User Input Analyser.py
+++ User Input Analyser.py
@@ -1,5 +1,5 @@
 #   Questionaire 1: Ask Directly
 #   Questionaire 2: Use Generic Algorithm to calculate % rate of Risk
 
-User_risk_level = 90
+User_risk_level = "medium"
 print("User Risk Level is: ", User_risk_level)
